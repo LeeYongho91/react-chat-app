d02a2b8b79e9f99e8e57b88c75b254276fcaa3d8
Essence <skyks4848@naver.com>
2021-11-14 21:40:57 +0900

    20211114

--- src/index.js
+++ src/index.js
@@ -36,23 +36,49 @@ ReactDOM.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
+// 시간복잡도 O(n^2)
+function compareMaps(map1, map2) {
 
+  if(map1.size !== map2.size) return false;
 
-function solution(n,k,arr) {
-
-  let answer = Number.MIN_SAFE_INTEGER;
-  let sum = 0;
-
-  // 슬라이딩 윈도우 
-  for (let i = 0; i < k; i++) {
-    sum += arr[i];
+  for(let [key, val] of map1) {
+      if(!map2.has(key) || map2.get(key) !== val) return false;
+  }
+  return true;  
 }
 
-  for (let i = k; i < arr.length; i++) {
-      sum += (arr[i] - arr[i-k]);
-      answer = Math.max(answer, sum);
+function solution(s, t) {
+
+  let answer = 0;
+  let th = new Map();
+  let sh = new Map();
+
+  for(let x of t) {
+      if(th.has(x)) th.set(x, th.get(x) + 1);
+      else th.set(x, 1);
   }
+
+  let len = t.length - 1;
+
+  for (let i = 0; i < len; i++) {
+      if(sh.has(s[i])) sh.set(s[i], sh.get(s[i]) + 1);
+      else sh.set(s[i], 1);
+  }
+
+  let lt = 0;
+  
+  for (let rt = len; rt < s.length; rt++) {
+      if(sh.has(s[rt])) sh.set(s[rt], sh.get(s[rt]) + 1);
+      else sh.set(s[rt], 1);
+
+      if(compareMaps(sh, th)) answer++;
+  
+      sh.set(s[lt], sh.get(s[lt]) - 1);
+      if(sh.get(s[lt]) === 0) sh.delete(s[lt]);
+      lt++;
+  }
+
   return answer;
 }
 
-console.log(solution(10,3,[12,15,11,20,25,10,20,19,13,15]));
+console.log(solution('bacaAacba', 'abc'));
